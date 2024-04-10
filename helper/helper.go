7c35cf180d3092923f89d5c734cb174c46d81401
farismnrr <farismunir2@gmail.com>
2024-04-11 00:07:51 +0700

    add function to consume API

--- helper/helper.go
+++ helper/helper.go
@@ -4,41 +4,11 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
-	"os/exec"
-	"runtime"
-	"time"
 
 	"github.com/farismnrr/go-auth-api-consume/model"
 )
-
-func ClearScreen() {
-	osName := runtime.GOOS
-
-	switch osName {
-	case "linux", "darwin": // Untuk Linux dan MacOS
-		cmd := exec.Command("clear")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	case "windows": // Untuk Windows 10
-		cmd := exec.Command("cmd", "/c", "cls")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	default:
-		fmt.Println("Clear screen tidak didukung pada sistem operasi ini")
-	}
-}
-
-func Delay(duration int) {
-	for i := duration; i >= 1; i-- {
-		fmt.Printf("\r%d seconds...", i)
-		time.Sleep(1 * time.Second)
-	}
-	fmt.Print("\r")
-}
 
 func GenerateHash(text string) string {
 	hasher := md5.New()
