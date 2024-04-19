dd76a484e3ff5d21535ba3bcf80dbc1883aa8f34
farismnrr <farismunir2@gmail.com>
2024-04-19 08:37:45 +0700

    add JWT logic

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/farismnrr/golang-authorization-api
 
-go 1.22.1
+go 1.18
--- handler/handler.go
+++ handler/handler.go
@@ -9,12 +9,42 @@ import (
 	"github.com/farismnrr/golang-authorization-api/model"
 )
 
-func GetDataFromAPI() (*model.ResponseData, error) {
+func GetKeyFromAPI() (string, error) {
 	_, authToken := helper.ReadJsonFile()
 
-	req, err := http.NewRequest("GET", "tinyurl.com/farismnrr-copyright", nil)
+	req, err := http.NewRequest("GET", "https://ruangguru-exercise.as.r.appspot.com/get-key", nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create request: %v", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+authToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	var response model.ResponseData
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return "", fmt.Errorf("failed to decode response body: %v", err)
+	}
+
+	return response.Data[0].CopyrightAuthorization, nil
+}
+
+func GetDataFromAPI() (*model.ResponseData, error) {
+	req, err := http.NewRequest("GET", "https://ruangguru-exercise.as.r.appspot.com/copyright", nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	authToken, err := GetKeyFromAPI()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return nil, err
 	}
 
 	req.Header.Set("Authorization", "Bearer "+authToken)
