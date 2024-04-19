45cadde7821ac3ddc9e001ad208fa0871debde59
farismnrr <farismunir2@gmail.com>
2024-04-19 11:55:53 +0700

    Change the response

--- handler/handler.go
+++ handler/handler.go
@@ -10,41 +10,14 @@ import (
 	"github.com/farismnrr/golang-authorization-api/model"
 )
 
-func GetKeyFromAPI() (string, error) {
-	_, authToken := helper.ReadJsonFile()
-
-	req, err := http.NewRequest("GET", "https://ruangguru-exercise.as.r.appspot.com/get-key", nil)
+func GetKeyFromAPI() (*model.ResponseData, error) {
+	_, authToken, err := helper.ReadJsonFile()
 	if err != nil {
-		return "", fmt.Errorf("failed to create request: %v", err)
+		fmt.Println("Gagal membaca file JSON:", err)
 	}
 
-	req.Header.Set("Authorization", "Bearer "+authToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	req, err := http.NewRequest("GET", "https://authorization-api-dot-ruangguru-exercise.as.r.appspot.com/get-key", nil)
 	if err != nil {
-		return "", fmt.Errorf("failed to send request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	var response model.ResponseData
-	err = json.NewDecoder(resp.Body).Decode(&response)
-	if err != nil {
-		return "", fmt.Errorf("failed to decode response body: %v", err)
-	}
-
-	return response.Data[0].CopyrightAuthorization, nil
-}
-
-func GetDataFromAPI() (*model.ResponseData, error) {
-	req, err := http.NewRequest("GET", "https://ruangguru-exercise.as.r.appspot.com/copyright", nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create request: %v", err)
-	}
-
-	authToken, err := GetKeyFromAPI()
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
 		return nil, err
 	}
 
@@ -53,36 +26,92 @@ func GetDataFromAPI() (*model.ResponseData, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %v", err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	var response model.ResponseData
 	err = json.NewDecoder(resp.Body).Decode(&response)
 	if err != nil {
-		return nil, fmt.Errorf("failed to decode response body: %v", err)
+		return nil, err
 	}
 
 	return &response, nil
 }
 
-func CopyrightHandler() {
-	response, err := GetDataFromAPI()
+func GetDataFromAPI() (*model.ResponseData, error) {
+	responseAPI, err := GetKeyFromAPI()
 	if err != nil {
-		// Handle error
-		log.Fatal("Error:")
+		return nil, err
 	}
 
-	username, _ := helper.ReadJsonFile()
+	req, err := http.NewRequest("GET", "https://authorization-api-dot-ruangguru-exercise.as.r.appspot.com/copyright", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(responseAPI.Data) > 0 {
+		lastData := responseAPI.Data[len(responseAPI.Data)-1]
+		req.Header.Set("Authorization", "Bearer "+lastData.CopyrightAuthorization)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var response model.ResponseData
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+func CopyrightHandler() bool {
+	responseAPI, err := GetDataFromAPI()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return false
+	}
+
+	responseJWT, err := GetKeyFromAPI()
+	if err != nil {
+		fmt.Println("Gagal mendapatkan kunci dari API:", err)
+		return false
+	}
+
+	username, _, err := helper.ReadJsonFile()
+	if err != nil {
+		fmt.Println("Gagal membaca file JSON:", err)
+		return false
+	}
 
 	hashed := helper.GenerateHash(username)
 
-	for _, userData := range response.Data {
-		if hashed == userData.CopyrightAuthorization {
-			log.Fatal("Copyright Authorized by", userData.Username)
+	authorized := false
+	for _, data := range responseAPI.Data {
+		if hashed == data.CopyrightAuthorization {
+			fmt.Println("Status:", responseJWT.Status)
+			fmt.Println(responseJWT.Message)
+			log.Println("Copyright Authorized by", data.Username)
+			authorized = true
+			break
 		} else {
-			log.Fatal("Unauthorized! Please contact the owner's code")
+			fmt.Println("Status: 401")
+			fmt.Println("message:", username, "is not authorized!")
+			return false
 		}
 	}
 
+	if !authorized {
+		fmt.Println("Status:", responseAPI.Status)
+		fmt.Println(responseJWT.Message)
+		fmt.Println("Message:", responseAPI.Message)
+	}
+
+	return authorized
 }
--- helper/helper.go
+++ helper/helper.go
@@ -16,7 +16,7 @@ func GenerateHash(text string) string {
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
-func ReadJsonFile() (string, string) {
+func ReadJsonFile() (string, string, error) {
 	authData, err := ioutil.ReadFile("Authorization.json")
 	if err != nil {
 		log.Fatal("Please insert the authorization file!")
@@ -25,5 +25,5 @@ func ReadJsonFile() (string, string) {
 	var auth model.AuthorizationData
 	json.Unmarshal(authData, &auth)
 
-	return auth.Username, auth.PrivateKey
+	return auth.Username, auth.PrivateKey, nil
 }
