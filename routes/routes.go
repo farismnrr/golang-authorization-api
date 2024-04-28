ccaef311a6d575c548d75db3e06db2867b02b4a8
farismnrr <farismunir2@gmail.com>
2024-04-29 03:58:22 +0700

    update routes endpoint

--- routes/routes.go
+++ routes/routes.go
@@ -3,30 +3,35 @@ SetupRoutes function sets up the routes for the application.
 It takes a Gin router and a CopyrightController instance as parameters.
 
 Routes:
-- GET /get-key: Retrieves the private key for authorization using middleware.
 - GET /: Returns server information.
-- GET /copyright: Retrieves copyright.
-- POST /copyright: Adds new copyright.
-- PUT /copyright: Updates existing copyright.
-- DELETE /copyright: Removes copyright.
+- GET /get-key: Retrieves the private key for authorization using middleware.
+- GET /{apiVersion}/copyrights: Retrieves copyright.
+- POST /{apiVersion}/copyrights: Adds new copyright.
+- PUT /{apiVersion}/copyrights: Updates existing copyright.
+- DELETE /{apiVersion}/copyrights: Removes copyright.
 */
 
 package routes
 
 import (
+	"fmt"
+
 	"github.com/farismnrr/golang-authorization-api/controller"
 	"github.com/farismnrr/golang-authorization-api/middleware"
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(router *gin.Engine, controller *controller.CopyrightController) {
-	{
-		router.GET("/get-key", middleware.AuthorizationMiddleware((middleware.AuthorizationConfig().PrivateKey)), controller.ShowCloudflareResponse)
+func SetupRoutes(router *gin.Engine, controller *controller.CopyrightController, apiVersion string) {
+	apiGroup := router.Group(fmt.Sprintf("/%s", apiVersion))
 
-		router.GET("/", controller.GetServer)
-		router.GET("/copyright", controller.GetCopyright)
-		router.POST("/copyright", controller.AddCopyright)
-		router.PUT("/copyright", controller.UpdateCopyright)
-		router.DELETE("/copyright", controller.RemoveCopyright)
+	apiGroup.GET("/", controller.GetServer)
+	apiGroup.GET("/get-key", middleware.AuthorizationMiddleware((middleware.AuthorizationConfig().PrivateKey)), controller.ShowCloudflareResponse)
+
+	copyrightGroup := apiGroup.Group("/copyrights")
+	{
+		copyrightGroup.GET("/", controller.GetCopyright)
+		copyrightGroup.POST("/", controller.AddCopyright)
+		copyrightGroup.PUT("/", controller.UpdateCopyright)
+		copyrightGroup.DELETE("/", controller.RemoveCopyright)
 	}
 }
